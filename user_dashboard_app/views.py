78c4df801275b40035814766aa4cf2d312e97bb2
Chris <cmsl4y3r@gmail.com>
2021-08-26 20:14:56 -0600

    register.html, signin.html and main navigation set up

--- user_dashboard_app/views.py
+++ user_dashboard_app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html' )
+
+def register(request):
+    return render(request, 'register.html')
+
+def signin(request):
+    return render(request, 'signin.html')
